8fbbc7bfd98c7cfaff3f6d52ff782923fb159b99
abhisheksurya8732 <abhisheksurya2005@gmail.com>
2025-03-14 15:18:35 +0530

    Update js.js

--- js.js
+++ js.js
@@ -43,44 +43,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // count-end 
 
-// facilities 
 
-function togglePopup() {
-    const popupOverlay = document.getElementById('facilityPopup');
-    popupOverlay.classList.toggle('active');
-}
-// facility end 
-
-// feedback 
-function submitFeedback(studentId) {
-    const feedbackInput = document.getElementById(`feedback-input-${studentId}`).value;
-    const nameInput = document.getElementById(`name-input-${studentId}`).value;
-    const selectedRating = document.querySelector(`input[name="rating-${studentId}"]:checked`);
-
-    if (feedbackInput && selectedRating && nameInput) {
-        const feedbackContainer = document.getElementById('feedback-container');
-
-        const newCard = document.createElement('div');
-        newCard.classList.add('card');
-        newCard.innerHTML = `
-  <div class="inner-card">
-    <div class="front">
-      <h3>Feedback</h3>
-      <p>${feedbackInput}</p>
-      <p>Rating: ${selectedRating.value} stars</p>
-      <p>By: ${nameInput}</p>
-    </div>
-  </div>
-`;
-
-        feedbackContainer.appendChild(newCard);
-        feedbackContainer.scrollLeft = feedbackContainer.scrollWidth;
-
-        document.getElementById(`feedback-input-${studentId}`).value = "";
-        document.getElementById(`name-input-${studentId}`).value = "";
-        document.querySelectorAll(`input[name="rating-${studentId}"]`).forEach(radio => radio.checked = false);
-    } else {
-        alert("Please provide your name, feedback, and rating!");
-    }
-}
-// feedback end 
